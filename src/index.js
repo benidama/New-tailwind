7aa415894cd7a2d7ee5db918eddb2addcc1bf8fa
benidama <benidama02@gmail.com>
2025-03-12 20:30:49 +0000

    All added

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const num = [12, 45, 66, 75];
+function Add(a, d) {
+  console.log(a + d);
+}
+Add(78, 34);
+
+// function RestFUN(...numbers) {
+//   var Sum = 0;
+//   for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] % 2 === 0) Sum = Sum + numbers[i];
+//   }
+//   return Sum;
+// }
+
+// console.log(RestFUN(34, 23, 6, 90, 32, 19));
