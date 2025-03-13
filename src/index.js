d00303cf13eb88019befc74f56f45d70ea289be8
benidama <benidama02@gmail.com>
2025-03-13 13:00:05 +0000

    Adding content

--- src/index.js
+++ src/index.js
@@ -4,12 +4,22 @@ function Add(a, d) {
 }
 Add(78, 34);
 
-// function RestFUN(...numbers) {
-//   var Sum = 0;
-//   for (let i = 0; i < numbers.length; i++) {
-//     if (numbers[i] % 2 === 0) Sum = Sum + numbers[i];
-//   }
-//   return Sum;
-// }
+//Rest parameter
+function RestFUN(...numbers) {
+  var Sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) Sum = Sum + numbers[i];
+  }
+  return Sum;
+}
+console.log(RestFUN(34, 23, 6, 90, 32, 19));
 
-// console.log(RestFUN(34, 23, 6, 90, 32, 19));
+//closure
+function closureFun(el1) {
+  console.log(el1);
+  function Amabuye() {
+    console.log("Nibyo kbx ntakubeshya kurimo.");
+  }
+  Amabuye();
+}
+closureFun("Ese koko nibyo cg nukubeshya?");
