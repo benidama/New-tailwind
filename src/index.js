d2c68813f42ef8d07e026850bbd95f50ef744cd5
benidama <benidama02@gmail.com>
2025-03-22 13:11:32 +0000

    Adding changes

--- src/index.js
+++ src/index.js
@@ -95,3 +95,24 @@ function last(x) {
   return wordsAlph;
 }
 console.log(last("If two words have the same last letter"));
+
+// Complete the method which accepts an array of integers, and returns one of the following:
+
+// "yes, ascending" - if the numbers in the array are sorted in an ascending order
+// "yes, descending" - if the numbers in the array are sorted in a descending order
+// "no" - otherwise
+// You can assume the array will always be valid, and there will always be one correct answer.
+
+function isSortedAndHow(array) {
+  for (i = 0; i <= array.length - 1; i++) {
+    if (array[i] <= array[i + 1]) {
+      return "yes, ascending";
+    }
+    if (array[i] >= array[i + 1] && array[i + 1] >= array[i + 2]) {
+      return "yes, descending";
+    } else {
+      return "no";
+    }
+  }
+}
+console.log(isSortedAndHow([78, 23, 123, 78, 35, 98]));
